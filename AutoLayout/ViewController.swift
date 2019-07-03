7a535019e6f1a69086cd41ac032f966568897ea0
Toof <letuananh.programming@gmail.com>
2019-07-03 13:44:21 +0700

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by TuanAnh on 7/3/19.
+//  Copyright © 2019 Toof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
